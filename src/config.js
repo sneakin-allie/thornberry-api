f081f9bb6fec4e2b817157f9aa9711b29ce49906
Allison Schulman <allison.d.schulman@gmail.com>
2021-07-07 15:54:48 -0400

    Removed extra / from url

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || `development`,
     DATABASE_URL: process.env.DATABASE_URL || `postgresql://allison_schulman@localhost/sightings`,
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || `postgresql://allison_schulman@localhost/sightings_test`,
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || `https://desolate-meadow-47002.herokuapp.com/`
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || `https://desolate-meadow-47002.herokuapp.com`
 };
